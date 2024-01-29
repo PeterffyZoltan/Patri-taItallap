4eb65bdd0e40b53f2f6dc9650646e85412cb969e
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2024-01-29 12:17:48 +0100

    smth

---
+++ BeerQuoran/ViewModel/Utils/ApiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace BeerQuoran.ViewModel.Helpers
+{
+    public class ApiHelper
+    {
+        public const string QuoranApiUrl = "https://api.alquran.cloud/v1/ayah/{0}/editions/quran-uthmani,en.pickthall";
+        public const string BeerApiUrl = "https://api.punkapi.com/v2/beers";
+
+
+
+
+
+    }
+}
