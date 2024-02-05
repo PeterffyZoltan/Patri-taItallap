40d9128e3628f3647eb62957fcab4005a30a5023
papp.zsombor <papp.zsombor@students.jedlik.eu>
2024-02-05 11:59:38 +0100

    ?

--- BeerQuoran/Model/Beer.cs
+++ BeerQuoran/Model/Beer.cs
@@ -52,7 +52,7 @@ namespace BeerQuoran.Model
     public class MashTemp
     {
         public Temp temp { get; set; }
-        public int duration { get; set; }
+        public bool? duration { get; set; }
     }
 
     public class Method
@@ -69,15 +69,15 @@ namespace BeerQuoran.Model
         public string tagline { get; set; }
         public string first_brewed { get; set; }
         public string description { get; set; }
-        public string image_url { get; set; }
-        public double abv { get; set; }
-        public int ibu { get; set; }
-        public int target_fg { get; set; }
-        public int target_og { get; set; }
-        public int ebc { get; set; }
-        public int srm { get; set; }
-        public double ph { get; set; }
-        public int attenuation_level { get; set; }
+        public string? image_url { get; set; }
+        public double? abv { get; set; }
+        public double? ibu { get; set; }
+        public double? target_fg { get; set; }
+        public double? target_og { get; set; }
+        public double? ebc { get; set; }
+        public double? srm { get; set; }
+        public double? ph { get; set; }
+        public bool attenuation_level { get; set; }
         public Volume volume { get; set; }
         public BoilVolume boil_volume { get; set; }
         public Method method { get; set; }
